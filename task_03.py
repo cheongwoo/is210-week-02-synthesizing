d47abb286d272d76db4bef93141acdaaac2eec0d
Cheongwoo Kim <cheong_woo.kim@spsmail.cuny.edu>
2015-02-08 00:19:14 +0000

    Added task_03

--- task_03.py
+++ task_03.py
@@ -1,7 +1,9 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is task_03"""
+
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
